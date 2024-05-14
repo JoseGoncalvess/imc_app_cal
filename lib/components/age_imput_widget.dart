f10fa827eef716e4e2a966883158ee0c14d7e594
JoseGoncalvess <joseinacio774@gmail.com>
2024-05-14 12:10:46 -0300

    created input age

---
+++ lib/components/age_imput_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AgeImputWidget extends StatefulWidget {
+  final TextEditingController controller;
+
+  const AgeImputWidget({super.key, required this.controller});
+
+  @override
+  State<AgeImputWidget> createState() => _AgeImputWidgetState();
+}
+
+class _AgeImputWidgetState extends State<AgeImputWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.06,
+      width: MediaQuery.sizeOf(context).width * 0.4,
+      decoration: const BoxDecoration(
+          color: Colors.orangeAccent,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: TextField(
+        inputFormatters: [LengthLimitingTextInputFormatter(3)],
+        onChanged: (value) {
+          setState(() {
+            widget.controller.text = value;
+          });
+        },
+        controller: widget.controller,
+        style: TextStyle(
+            fontSize: MediaQuery.of(context).size.width * 0.05,
+            fontWeight: FontWeight.bold,
+            color: Colors.white),
+        maxLines: 1,
+        decoration: InputDecoration(
+            hintText: "Idade",
+            hintStyle: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+            border: InputBorder.none),
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
