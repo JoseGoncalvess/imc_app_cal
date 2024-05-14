8c577e030658adc0c8592b6867d9dc7131347b45
JoseGoncalvess <joseinacio774@gmail.com>
2024-05-14 09:26:06 -0300

    create dorpButtom

---
+++ lib/components/drop_measures_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class DropMeasuresWidget extends StatefulWidget {
+  final List<String> measures;
+  const DropMeasuresWidget({super.key, required this.measures});
+
+  @override
+  State<DropMeasuresWidget> createState() => _DropMeasuresWidgetState();
+}
+
+class _DropMeasuresWidgetState extends State<DropMeasuresWidget> {
+  late String dropDowValue;
+  @override
+  void initState() {
+    super.initState();
+    dropDowValue = widget.measures.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.06,
+      width: MediaQuery.sizeOf(context).width * 0.4,
+      decoration: const BoxDecoration(
+          color: Colors.orangeAccent,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.05,
+            width: MediaQuery.sizeOf(context).width * 0.22,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 3.0),
+              child: TextField(
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+                maxLines: 1,
+                decoration: const InputDecoration(border: InputBorder.none),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+              ),
+            ),
+          ),
+          DropdownButton(
+            value: dropDowValue,
+            elevation: 2,
+            dropdownColor: Colors.orangeAccent,
+            isDense: true,
+            items: widget.measures
+                .map<DropdownMenuItem<String>>(
+                    (String value) => DropdownMenuItem(
+                          value: value,
+                          child: Text(value,
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.03,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white)),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                dropDowValue = value!;
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_view_model.dart
+++ lib/pages/home_view_model.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:imc_app_cal/pages/home/home.dart';
 
 abstract class HomeViewModel extends State<Home> {
+List<String> tamanho = ["cm","ml","m"];
+List<String> pesos = ["kg","g","mg"];
 final name = TextEditingController();
 final age = TextEditingController();
 final heith = TextEditingController();
