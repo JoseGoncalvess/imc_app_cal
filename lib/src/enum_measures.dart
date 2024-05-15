51b9c493704b1f93c053ccb8c8cf8254892ed4c9
JoseGoncalvess <joseinacio774@gmail.com>
2024-05-15 17:34:03 -0300

    created enum measures

---
+++ lib/src/enum_measures.dart
@@ -0,0 +1,17 @@
+
+enum Measureswidth{
+kg(name:"KG"),
+g(name:"G");
+const Measureswidth({required this.name});
+final String name;
+
+}
+
+enum Measuresheight{
+  cm(name: "CM"),
+  m(name: "M");
+
+  const Measuresheight({required this.name});
+  final String name;
+}
+
